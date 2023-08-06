428bba972261f0eb801806a2274705f097082544
abhikatyare <katyare.abhishek@gmail.com>
2023-08-06 13:44:01 -0700

    Updates

--- js/index.js
+++ js/index.js
@@ -1,12 +1,15 @@
 products.slice(0,2).forEach(product =>{
     const productDiv = document.createElement('div');
-    productDiv.setAttribute("class", "product-card")
+    productDiv.setAttribute("class", "product-card-home")
     productDiv.innerHTML = 
     `
         <img class="product-card-img" src="${product.images[0]}" alt="Product Image"></img> 
-        <div>
+        <div class="product-card-desc">
             <h6>${product.title}</h6>
             <p>${product.subTitle}</p>
+            <div>
+                <button class="btn btn-custom">Read More</button>
+            </div>
         <div>
     `;
     document.getElementById('featured-products').appendChild(productDiv);
