d4124f5c369e708ea06b44a32be04935fb773afb
Abhishek Katyare <32732170+abhikatyare@users.noreply.github.com>
2024-05-12 00:25:13 +0000

    Updates

--- js/index.js
+++ js/index.js
@@ -19,11 +19,8 @@ products.slice(0,2).forEach((product, i) =>{
 
 
 const scrollers = document.querySelectorAll(".scroller");
+addAnimation();
 
-// If a user hasn't opted in for recuded motion, then we add the animation
-if (!window.matchMedia("(prefers-reduced-motion: reduce)").matches) {
-  addAnimation();
-}
 
 function addAnimation() {
   scrollers.forEach((scroller) => {
